3a4c1fa6db44624a9a2869855bac0784ecdf320c
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-15 12:04:34 +0800

    Update welcome message in root endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This test is brought to you by Accenture Philippines");
+app.MapGet("/", () => "This test is brought to you by Accenture Philippines!");
 
 app.Run();
