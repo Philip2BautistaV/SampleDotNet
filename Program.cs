e2be83fda13bbef27bf978bb04afff96bf1bc342
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-15 15:37:18 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Welcome to Accenture Cubao");
+app.MapGet("/", () => "Testing only");
 
 app.Run();
