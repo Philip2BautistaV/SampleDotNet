64d1a098f9a4761bf2e36b0343e5158ec4b887db
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-18 23:54:53 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Test Branching Strategy - 061826");
+app.MapGet("/", () => "This test is for DataPlatform");
 
 app.Run();
