1eda416ff835328f90b6162454877a520639c3ca
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-18 15:15:31 +0800

    Update root endpoint response message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "1st testing for today");
+app.MapGet("/", () => "1st testing for Orchastration in DataPlatform");
 
 app.Run();
