f55c01f4d10ddd885233e4ca45ed80ee52628a7b
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-20 23:43:37 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Test for today - 062026");
+app.MapGet("/", () => "Welcome to DBG Project");
 
 app.Run();
