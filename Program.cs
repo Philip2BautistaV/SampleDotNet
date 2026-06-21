7383b19db9ebc8b036c0c47f3ff3d0e673461136
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-21 16:33:28 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Error Fixed");
+app.MapGet("/", () => "DataPlatform Completed");
 
 app.Run();
