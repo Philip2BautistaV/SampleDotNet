d10f75d5eafd0fd74a7e0967b78d454d14e35849
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-24 15:21:40 +0800

    Update root endpoint response message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "DBG pipeline test - 062426");
+app.MapGet("/", () => "WebApp deployment completed");
 
 app.Run();
