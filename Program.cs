f1f6c17c177415d2c77f77fb5544f8d2e7153b6f
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-24 15:22:27 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "DBG pipeline test - 062426");
+app.MapGet("/", () => "AI/ML deployment completed");
 
 app.Run();
