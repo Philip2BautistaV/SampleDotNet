d90e0c2fe2449883e58b7d9ff7dcb323b14ca983
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-07-03 15:52:25 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "DBG Test Feature");
+app.MapGet("/", () => "121212121212121212");
 
 app.Run();
