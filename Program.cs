2f016b54666af11275c16207242e23af88ec01f3
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-07-03 16:03:15 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "AIML 2nd build");
+app.MapGet("/", () => "Failed in deployment");
 
 app.Run();
